2683bc294e9e5277001d61647b202c053ae06c11
Lucas de Matos Figueredo <lucas@lucass-macbook-pro-2.local>
2016-03-10 22:20:17 -0800

    Finishing project 2 pt1

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -9,30 +9,73 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var secretWord: UILabel!
+    var chosen = [Character]()
+    var phrase = ""
+    
+    @IBOutlet weak var hangmanImage: UIImageView!
+    var imageIndex = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()
         print(phrase)
+        secretWord.text = ""
+        
+        for c in phrase.characters {
+            
+            if c == " " {
+                secretWord.text! += "  "
+            }
+            else {
+                secretWord.text! += "_  "
+            }
+        }
     }
-
+    
+    @IBAction func letterGuess(sender: UIButton) {
+        
+        
+        sender.enabled = false
+        let newC = Character(sender.currentTitle!)
+        if(!phrase.characters.contains(newC)) {
+            imageIndex++
+            hangmanImage.image = UIImage(named: "hangman\(min(max(1, imageIndex), 7)).gif")
+        }
+        chosen.append(newC)
+        var newSecretWord = ""
+        for c in phrase.characters {
+            if c == " " {
+                newSecretWord += "  "
+            }
+            else if (chosen.contains(c)){
+                newSecretWord += String(c) + "  "
+            }
+            else {
+                newSecretWord += "_  "
+            }
+        }
+        secretWord.text = newSecretWord
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
